7a2369cb8449d3f8e04562ddcb97505bbb7c1e9f
Saketh Gunnam <gunnamsrisaketh@gmail.com>
2024-05-16 10:52:00 +0530

    Resolve merge conflict by keeping both sum methods

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,5 +1,11 @@
 public class Calculate {
+    // Original sum method
     public int sum(int a, int b) {
-        return a + b+10;
+        return a + b;
+    }
+
+    // Updated sum method from feature branch
+    public int sumPlusTen(int a, int b) {
+        return a + b + 10;
     }
 }
